f9daeaf4ef76cead12fbcf89b3aab6d57c8cf4ac
Adrian Kodjasz <34575120+buhaay@users.noreply.github.com>
2018-02-08 15:47:57 +0100

    Update page_report.py

--- page_report.py
+++ page_report.py
@@ -1,40 +1,27 @@
 import csv
 import re
 import operator
-
 import sys
 
-logs = """
-10.4.180.222 [28/Jan/2018:10:02:32 +0100] "GET http://clearcode.cc/ HTTP/1.1" 200 1080
-10.4.180.222 [28/Jan/2018:10:03:31 +0100] "GET http://www.clearcode.cc HTTP/1.1" 200 3056
-10.4.180.222 [28/Jan/2018:10:05:30 +0100] "GET http://clearcode.cc/careers HTTP/1.1" 200 3056
-10.4.180.222 [28/Jan/2018:10:08:29 +0100] "GET http://clearcode.cc/careers/ HTTP/1.1" 200 3056
-10.4.180.222 [28/Jan/2018:10:13:29 +0100] "GET http://clearcode.cc/careers? HTTP/1.1" 200 3056
-10.4.180.222 [28/Jan/2018:10:21:27 +0100] "GET http://clearcode.cc/careers/? HTTP/1.1" 200 3056
-10.4.180.222 [28/Jan/2018:10:34:26 +0100] "GET
-http://clearcode.cc/careers?offer=internship&type=python HTTP/1.1" 200 4545
 
-10.4.180.222 [28/Jan/2018:10:55:25 +0100] "GET
-http://clearcode.cc/careers?type=frontend&offer=internship HTTP/1.1" 200 5454
-https://www.google.pl/
-https://www.google.pl/
-https://docs.python.org/3/library/
-https://docs.python.org/3/library/
-https://docs.python.org/3/library/
-
-"""
-
-urls = re.findall(r'(?P<url>[a-z]*[a-z]+\.[a-z]+/*[a-z]*\.*[a-z]+)', logs)
-
+invalid_logs = {"number": 0}
 dict = {}
-for url in urls:
-    if url in dict:
-        dict[url] += 1
-    else:
-        dict.setdefault(url, 1)
+
+with open("logs.log") as log_report:
+    for line in log_report:
+        correct_log = re.findall(r'([(\d\.)]+) \[(.*?)\] "(.*?)" (\d+) (\d+)', line)
+
+        if not correct_log:
+            invalid_logs["number"] += 1
+        else:
+            stripped_url = re.findall(r'(?P<url>[a-z]*[a-z]+\.[a-z]+/*[a-z]*\.*[a-z]+)', str(correct_log))
+            dict.setdefault(stripped_url[0], 0)
+            dict[stripped_url[0]] += 1
 
 
-spamwriter = csv.writer(sys.stdout, quoting=csv.QUOTE_NONNUMERIC)
+csv_writer = csv.writer(sys.stdout, quoting=csv.QUOTE_NONNUMERIC)
 
-spamwriter.writerows(sorted(dict.items(),
+csv_writer.writerows(sorted(dict.items(),
                             key=operator.itemgetter(1), reverse=True))
+
+sys.stderr.write("Invalid log lines: {}\n".format(invalid_logs["number"]))
