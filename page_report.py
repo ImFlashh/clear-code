5152259b04eae9b6ec26d629684ef3ed7965eb55
Adrian Kodjasz <34575120+buhaay@users.noreply.github.com>
2018-02-08 18:10:16 +0100

    Update page_report.py
    zmodyfikowane do jednej pętli

--- page_report.py
+++ page_report.py
@@ -4,24 +4,24 @@ import operator
 import sys
 
 
-invalid_logs = {"number": 0}
-dict = {}
+invalid_logs = 0
+correct_logs = {}
 
 with open("logs.log") as log_report:
     for line in log_report:
         correct_log = re.findall(r'([(\d\.)]+) \[(.*?)\] "(.*?)" (\d+) (\d+)', line)
 
         if not correct_log:
-            invalid_logs["number"] += 1
+            invalid_logs += 1
         else:
             stripped_url = re.findall(r'(?P<url>[a-z]*[a-z]+\.[a-z]+/*[a-z]*\.*[a-z]+)', str(correct_log))
-            dict.setdefault(stripped_url[0], 0)
-            dict[stripped_url[0]] += 1
+            correct_logs.setdefault(stripped_url[0], 0)
+            correct_logs[stripped_url[0]] += 1
 
 
 csv_writer = csv.writer(sys.stdout, quoting=csv.QUOTE_NONNUMERIC)
 
-csv_writer.writerows(sorted(dict.items(),
+csv_writer.writerows(sorted(correct_logs.items(),
                             key=operator.itemgetter(1), reverse=True))
 
-sys.stderr.write("Invalid log lines: {}\n".format(invalid_logs["number"]))
+sys.stderr.write("Invalid log lines: {}\n".format(invalid_logs))
